49eb7a8def4cdfe4201bb94196a36c87126f077d
algothinktank <65403794+algothinktank@users.noreply.github.com>
2020-05-15 13:43:03 -0400

    Add files via upload

---
+++ Back_tester.py
@@ -0,0 +1,28 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime
+
+import backtrader as bt
+import backtrader.analyzers as btanalyzers
+import backtrader.feeds as btfeeds
+import backtrader.strategies as btstrats
+
+cerebro = bt.Cerebro()
+
+# data
+dataname = 'yahoo1.csv'
+data = btfeeds.BacktraderCSVData(dataname=dataname)
+
+cerebro.adddata(data)
+
+# strategy
+cerebro.addstrategy(btstrats.SMA_CrossOver)
+
+# Analyzer
+cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
+
+thestrats = cerebro.run()
+thestrat = thestrats[0]
+
+print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
